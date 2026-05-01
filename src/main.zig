40f3d683c562c1cff29366b57cab76add53c8f27
andresm.rodriguezcan <andresm.rodriguezcan@gmail.com>
2026-05-01 01:22:51 -0400

    Read program source from Index.txt and update lifetimes
    Switch the entry point from a hardcoded expression to @embedFile of
    src/Index.txt and rewire ownership for the new APIs: free the
    statement slice element-by-element, deinit the interpreter, and add
    a sample let-based program in Index.txt.

--- src/main.zig
+++ src/main.zig
@@ -7,18 +7,24 @@ const Io = std.Io;
 const Z = @import("Z");
 
 pub fn main(init: std.process.Init) !void {
+    const allocator = init.gpa;
     var c_buffer: [4096]u8 = undefined;
     var c_writer = std.Io.File.stdout().writer(init.io, &c_buffer);
 
-    var lex = Lexer.init("21 * 2");
+    var lex = Lexer.init(@embedFile("./Index.txt"));
     var parser = Parser.init(&lex);
-    var ast = try parser.parse(init.gpa);
-    defer ast.deinit(init.gpa);
-    var interpreter = Interpreter.init();
-    const results = try interpreter.execute(ast, init.gpa);
-    defer init.gpa.free(results);
+    const ast = try parser.parse(allocator);
+    defer {
+        for (ast) |stmt| stmt.deinit(allocator);
+        allocator.free(ast);
+    }
+
+    var interpreter = Interpreter.init(allocator);
+    defer interpreter.deinit();
+
+    const results = try interpreter.execute(ast, allocator);
+    defer allocator.free(results);
 
     try c_writer.interface.print("{s}", .{results});
-
-    try c_writer.interface.flush(); // Don't forget to flush!!!!
+    try c_writer.interface.flush();
 }
